94803d0f1461c8ec0d1116a70f4bd1315d822bb1
Lilkhar <vxy1099@gmail.com>
2024-05-29 04:41:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Playing  Kingdom SMP: http://localhost:${port}`);
   console.log(`🔗 Powered By RTX`);
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["PLAYING","Kingdom SMP"];
 
 
 let currentIndex = 0;
